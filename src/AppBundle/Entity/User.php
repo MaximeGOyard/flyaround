d9ea9c282b851fa99f2a07c6ec814431a77602d0
maxime goyard <maxime.goyard@live.fr>
2018-05-16 11:24:52 +0200

    creation du FOSUser

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -3,14 +3,15 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
 
 /**
  * User
  *
- * @ORM\Table(name="user")
+ * @ORM\Table(name="`user`")
  * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
  */
-class User
+class User extends BaseUser
 {
     /*
      * Adding personal methods / variables
@@ -45,7 +46,7 @@ class User
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      */
-    private $id;
+    protected $id;
 
     /**
      * @var string
@@ -279,6 +280,7 @@ class User
      */
     public function __construct()
     {
+        parent::__construct();
         $this->reviewAuthors = new \Doctrine\Common\Collections\ArrayCollection();
         $this->pilots = new \Doctrine\Common\Collections\ArrayCollection();
         $this->passengers = new \Doctrine\Common\Collections\ArrayCollection();
